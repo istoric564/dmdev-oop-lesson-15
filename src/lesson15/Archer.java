54bbeffee9d42fc26e2de83ac5dd329ef1dcc900
istoric564 <mrdizzy38@gmail.com>
2021-03-15 22:19:39 +0700

    Java для начинающих. 16.1 Внутренние и вложенные классы

--- src/lesson15/Archer.java
+++ src/lesson15/Archer.java
@@ -3,13 +3,30 @@ package lesson15;
 import java.util.Random;
 
 public class Archer extends Hero {
+    private Wolf wolf;
+
     public Archer(String name, int damage, int health) {
         super(name, damage, health);
+        this.wolf = new Wolf("Wold", 7);
+
     }
 
     @Override
     public void attackEnemy(Enemy enemy){
         System.out.println(getName() + " выпустил три стрелы в " + enemy.getName() );
-        enemy.takeDamage(getDamage());
+        wolf.attackEnemy(enemy);
+    }
+    private class Wolf {
+        private String name;
+        private int damage;
+
+        public Wolf(String name, int damage) {
+            this.name = name;
+            this.damage = damage;
+        }
+        public void attackEnemy(Enemy enemy){
+            System.out.println(name + " и " + Archer.this.getName() + " наносят урон");
+            enemy.takeDamage(damage + Archer.this.getDamage());
+        }
     }
 }
