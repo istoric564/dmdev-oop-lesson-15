b0b530736680a5ff1afe4531ae132e2325004e07
istoric564 <mrdizzy38@gmail.com>
2021-03-17 00:50:54 +0700

    Java для начинающих. 19.3 ArrayList

---
+++ src/Lesson19/ArrayListRunner.java
@@ -0,0 +1,21 @@
+package Lesson19;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListRunner {
+//    [1, 2, 3, 4, 5]
+//    [1, 2, 3, 4, 5, null, null]
+    public static void main(String[] args) {
+        List<Integer> integers = new ArrayList<>(5);
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        integers.add(4);
+        integers.add(5);
+        integers.add(6);
+
+        System.out.println(integers.get(4));
+        System.out.println(integers.contains(4));
+    }
+}
